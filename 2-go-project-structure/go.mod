7f0a9dd8f1e05f24076d8795e48e322a973cf3a8
ehlui <86680816+ehlui@users.noreply.github.com>
2023-01-15 20:09:50 +0100

    Basic syntax with som examples and comments on go Adding the main current structure for a project based on go (using modules and static imports)

---
+++ 2-go-project-structure/go.mod
@@ -0,0 +1,5 @@
+module mymodule/basicus
+
+go 1.19
+
+require github.com/google/go-cmp v0.5.9
---
+++ 2-go-project-structure/hey.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"mymodule/basicus/morestrings"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+
+}
